510ff4d17b38d2d1ba864b787a59d8c8965db3fd
Taylan Alp Muhur <satanjager@gmail.com>
2026-01-01 23:34:55 +0100

    JS-1. bölüm

---
+++ JAVASCRIPT-Calisma/01_JS_nedir/js/01-hello-world.js
@@ -0,0 +1,17 @@
+// ** Merhaba Dünya Örnekleri **
+
+/* Açıklama satırı uzun satırlar sürecekse 
+bakınız
+*/
+
+// Dökümana (document) Yazdırma İşlemi ile Merhaba Dünya:  Döküman spesifik olmadığından consol'da yazmaz
+
+document.writeln('Merhaba Dünya - Dokumana yazdırma '); 
+
+// Uyari Mesajı (Alert) ile Merhaba Dünya: Alert consol'da yok yazmaz
+
+alert("Merhaba Dünya - Alert");  
+
+// console.log ile Merhaba Dünya: Konsolda yazma
+
+console.log("Merhaba Dünya - console.log");
